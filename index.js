2bdfa27a63d846278975d954429ea3be82f3b6b5
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-03 04:29:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
-//   this.training = training
-//
+ function BoardMember(name, homeState, training){
+   this.name = name
+   this.homeState = homeState
+   this.training = training
+}
+
 //   this.veto = function(){
 //     return 'No, I must disagree'
 //   }
