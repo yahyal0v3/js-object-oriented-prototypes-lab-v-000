92a2adaef66d192eae84aafe0369bcbfcc762682
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-03 04:31:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- function BoardMember(name, homeState, training){
+function BoardMember(name, homeState, training){
    this.name = name
    this.homeState = homeState
    this.training = training
@@ -7,18 +7,19 @@
 BoardMember.prototype.veto = function(){
      return 'No, I must disagree'
    }
-//   this.approve = function(){
-//     return 'You can do that!'
-//   }
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
-//
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
-// }
+
+BoardMember.prototype.approve = function(){
+     return 'You can do that!'
+   }
+BoardMember.prototype.doCharity = function(){
+     return "I like to help people."
+   }
+
+BoardMember.prototype.releasePressStatement = function(){
+     return "You will see great things from Scuber."
+   }
+
+BoardMember.prototype.sayHi = function(){
+     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+   }
+
